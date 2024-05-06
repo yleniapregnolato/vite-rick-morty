c4e85480356ca1d43109edd0fb457eb0e4eaa6cf
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-06 18:10:40 +0200

    fix: h3 font size

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -15,19 +15,24 @@ export default {
     <div class="ms_card">
         <img :src="image" alt="">
         <h2 class="text-center mt-3">{{ cardObj.name }}</h2>
-        <h3 class="fs-1 fw-lighter text-center mt-5">{{ cardObj.status }}</h3>
-        <h3 class="fs-1 fw-lighter text-center">{{ cardObj.species }}</h3>
+        <h3 class="fw-lighter text-center mt-5">{{ cardObj.status }}</h3>
+        <h3 class="fw-lighter text-center">{{ cardObj.species }}</h3>
     </div>
-
 </template>
 
 <style scoped lang="scss">
 @use "../style/partials/variables" as *;
+
 .ms_card {
     margin-top: 80px;
     color: $main-text-color;
+
     img {
-        border-radius: 50%;    
+        border-radius: 50%;
+    }
+
+    h3 {
+        font-size: 1.5rem;
     }
 }
 </style>
