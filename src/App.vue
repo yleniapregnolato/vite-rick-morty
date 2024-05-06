808b67f3437e4b125ae4f192c5d1270b91daff3e
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-06 16:14:27 +0200

    add: AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <script>
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
 export default {
-  data() {
-    return {
-
-    }
-  },
+    data() {
+        return {
+            cardsArray: [],
+        };
+    },
+    created() {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((resp) => {
+            this.cardsArray = resp.data.data;
+            // console.log(resp);
+        });
+    },
+    components: { AppHeader }
 };
 </script>
 
 <template>
-  
+  <AppHeader />
 </template>
 
 <style lang="scss">
+@use "./style/partials/variables" as *;
+
+template {
+    background-color: $background-color;
+}
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {};
+</script>
+
+<template>
+    <h1>Rick and Morty App</h1>
+</template>
+
+<style scoper lang="scss">
+@use "../style/partials/variables" as *;
+h1 {
+    color: $header-text-color;
+    font-weight: 800;
+    text-align: center;
+    padding-top: 50px;
+}
+</style>
